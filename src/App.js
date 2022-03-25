592918c02b42ac7fb6b651ec3180036878d46bc1
Jessy Jupiter <jessy@macbook-pro-de-jessy.home>
2022-03-25 13:43:05 +0100

    Keyboard Test 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState(null)
+
+  const keyLayout = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+    "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+    "space"
+  ];
+
+  const keyElementContainer = keyLayout.map(key => {
+    const insertLineBreak = ["backspace", "p", "enter", "?"]
+
+    if (insertLineBreak.includes(key)) {
+      return (
+        <Fragment>
+          <button type='button'>{key.toLowerCase()}</button>
+          <br />
+        </Fragment>
+      )
+    } else {
+      return <button type='button'>{key.toLowerCase()}</button>
+    }
+  })
+
+  const handleClick = () => {
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <textarea onClick={() => handleClick()} className="use-keyboard-input" cols="30" rows="10" onChange={() => setText(text)} value={text}></textarea>
+      <div className='keyboard", "keyboard--hidden'>
+        <article className='keyboard__keys'>
+          <Fragment>
+            {keyElementContainer}
+          </Fragment>
+        </article>
+      </div>
+    </main>
   );
 }
 
